99a7dcf22bafc83fecabec9fb7f3670286b71e65
Khilan <maradiyakhilan@gmail.com>
2024-06-23 10:27:00 +0530

    Add state recovery

--- chat-app/index.js
+++ chat-app/index.js
@@ -5,7 +5,11 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+
+// connectionStateRecovery is feature used to recover when it is disconnected temporary(temporary store the data)
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
 
 app.use(express.static(path.resolve("./public")));
 
@@ -15,10 +19,13 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
-    socket.on('chat message', (msg) => {
+    socket.on('chat message', (msg, callback) => {
         console.log('message: ' + msg);
         io.emit('chat message', msg);
-      });
+        callback({
+            status: 'ok'
+        });
+    });
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
